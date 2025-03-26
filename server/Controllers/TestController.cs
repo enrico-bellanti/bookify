633347d570fa27a2054c8e9fa96bbfeaf1711569
ebellanti <enrico.bellanti@nais-solutions.it>
2025-03-26 20:02:14 +0100

    updated controller

--- server/Controllers/TestController.cs
+++ server/Controllers/TestController.cs
@@ -8,10 +8,28 @@ namespace Bookify.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
+        IConfiguration _config;
+        public TestController(IConfiguration config)
+        {
+            _config = config;
+        }
+
         [HttpGet]
         public IActionResult Get()
         {
             return Ok();
+        }
+
+        [HttpGet("config")]
+        public IActionResult GetConfig()
+        {
+
+            var settings = new Dictionary<string, string>();
+            foreach (var kvp in _config.AsEnumerable())
+            {
+                settings[kvp.Key] = kvp.Value;
+            }
+            return Ok(settings);
         }
     }
 }
