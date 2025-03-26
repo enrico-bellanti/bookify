53bc9fda2b717b228527555cad27e614c1b9de95
ebellanti <enrico.bellanti@nais-solutions.it>
2025-03-26 18:09:06 +0100

    roll back

--- server/Program.cs
+++ server/Program.cs
@@ -195,14 +195,14 @@ builder.Services.AddHttpClient();
 var app = builder.Build();
 
 // Apply migrations at startup
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
-{
-    using (var scope = app.Services.CreateScope())
-    {
-        var dbContext = scope.ServiceProvider.GetRequiredService<YourDbContext>();
-        dbContext.Database.Migrate();
-    }
-}
+// if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+// {
+//     using (var scope = app.Services.CreateScope())
+//     {
+//         var dbContext = scope.ServiceProvider.GetRequiredService<YourDbContext>();
+//         dbContext.Database.Migrate();
+//     }
+// }
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
